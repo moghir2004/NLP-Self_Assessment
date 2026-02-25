12460b16cbed17fd83aa6a18cee3c52734ce3265
Kamal Moghir <moghir2004@gmail.com>
2026-02-25 23:34:07 +0100

    Cleaned up and reworked the logic to restric to nominal subjects and added gitignore for future commits

--- conll_reader.py
+++ conll_reader.py
@@ -24,17 +24,19 @@ class Tree:
 
     def isInv(self, nodes):
         verbs = {node.id:node.relation for node in nodes if node.pos == "V"}
+        eligible_verbs = set() # verbs with only nominal subjects
         postverbal_verbs = set()
         for node in nodes:
-            if node.relation == "suj" and node.head in verbs.keys():
+            if node.relation == "suj" and node.cpos == "N" and node.head in verbs.keys():
+                eligible_verbs.add(node.head)
                 if node.id > node.head:
                     postverbal_verbs.add(node.head)
         rel_clause_heads = {
-            node.id for node in nodes
-            if node.pos == "V" and node.relation == "mod.rel"
+            verb_id for verb_id in eligible_verbs
+            if self.nodes[verb_id].relation == "mod.rel"
         }
         postverbal_head_of_rel_clause = rel_clause_heads & postverbal_verbs
-        return len(verbs), len(postverbal_verbs), len(postverbal_head_of_rel_clause)
+        return len(eligible_verbs), len(postverbal_verbs), len(rel_clause_heads), len(postverbal_head_of_rel_clause)
 
 
 def reader(file_path):
@@ -68,17 +70,22 @@ conll_data = reader("sequoia.7.0.expanded.conll")
 total_verbs = 0
 postverbal_verbs = 0
 head_of_rel_clause = 0
+post_head_rel_clause = 0
 for sentence in conll_data:
     tree = Tree(sentence)
-    t, p, h = tree.isInv(sentence)
+    t, p, h, ph = tree.isInv(sentence)
     total_verbs += t
     postverbal_verbs += p
     head_of_rel_clause += h
+    post_head_rel_clause += ph
 
 probability_post = (postverbal_verbs / total_verbs) if total_verbs else 0.0
-probability_rel = (head_of_rel_clause / postverbal_verbs) if postverbal_verbs else 0.0
+probability_rel = (head_of_rel_clause / total_verbs) if total_verbs else 0.0
+probability_post_rel = (post_head_rel_clause / total_verbs) if total_verbs else 0.0
 print("total:", total_verbs)
 print("postverbal:", postverbal_verbs)
 print("head of rel clause", head_of_rel_clause)
+print("post_head_rel:", post_head_rel_clause)
 print("probability_post:", probability_post)
-print("probability_post_rel:", probability_rel)
+print("probability_rel:", probability_rel)
+print("probability_post_rel:", probability_post_rel)
--- probability_formulas.md
+++ probability_formulas.md
@@ -1,1 +1,28 @@
-$$P(REL|INV)=\frac{P(REL)}{P(INV)}$$
+Via restricting the set:
+$$
+\begin{align}
+    P(REL|INV) &= \frac{P(REL \cap INV)}{P(INV)}\\
+    &= \frac{\frac{18}{1179}}{\frac{69}{1179}} \\
+    &= \frac{18}{69}
+\end{align}
+$$
+$$
+\begin{align}
+    P(INV|REL) &= \frac{P(INV \cap REL)}{P(REL)}\\
+    &= \frac{\frac{18}{1179}}{\frac{58}{1179}} \\
+    &= \frac{18}{58}
+\end{align}
+$$
+
+Via Bayes' Theorem
+$$
+\begin{align}
+    P(INV|REL) &= \frac{P(REL|INV)\cdot P(INV)}{P(REL)} \\
+    &= \frac{\frac{18}{69}\cdot \frac{69}{1179}}{\frac{58}{1179}}\\
+    &= \frac{18}{58}
+
+\end{align}
+$$
+
+
+
