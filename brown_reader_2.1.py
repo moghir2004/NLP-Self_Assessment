d96094852595a7f439993e8468206e416c8cdcd7
Kamal Moghir <moghir2004@gmail.com>
2026-03-18 10:31:44 +0100

    cleanup

--- brown_reader_2.1.py
+++ brown_reader_2.1.py
@@ -8,7 +8,7 @@ def br_reader(file):
             items = line.strip().split(" ")
             for item in items:  # item = word/pos pair
                 # pairs = list of word then POS ([word,POS])
-                pairs = item.lower().split("/")
+                pairs = item.split("/")
                 if pairs[0] in collection.keys():  # increment if word
                     collection[pairs[0]] += 1  # is already in collection
                 else:
@@ -16,18 +16,10 @@ def br_reader(file):
         return collection
 
 
-arr = []
-sorted_col = {}
-
-
 def sort(collection: dict[str, int]):
-    l = sorted(
-        zip(collection, collection.values()),
-        reverse=True,
-        key=lambda x: x[1],
-    )
-    print(l[:99])  #
+    l = sorted(zip(collection, collection.values()), reverse=True, key=lambda x: x[1])
+    return l[:99]
 
 
 br_reader("emea-fr.brown")
-sort(collection)
+print(sort(collection))
